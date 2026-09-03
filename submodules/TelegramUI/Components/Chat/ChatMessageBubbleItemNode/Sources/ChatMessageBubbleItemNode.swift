2fdf1d775410c56cecbcf790f6bbed2462dc5ff8
Kai Tears <rei@Kais-MacBook-Air.local>
2026-09-03 23:13:06 +0200

    Remove broken HoldToShowOrReport message appendage

--- submodules/TelegramUI/Components/Chat/ChatMessageBubbleItemNode/Sources/ChatMessageBubbleItemNode.swift
+++ submodules/TelegramUI/Components/Chat/ChatMessageBubbleItemNode/Sources/ChatMessageBubbleItemNode.swift
@@ -366,32 +366,6 @@ private func contentNodeMessagesAndClassesForItem(_ item: ChatMessageItem) -> ([
                     
                     // MARK: Swiftgram
                     var message = message
-                    if message.canRevealContent(contentSettings: item.context.currentContentSettings.with { $0 }) {
-                        let originalTextLength = message.text.count
-                        let noticeString = i18n("Message.HoldToShowOrReport", item.presentationData.strings.baseLanguageCode)
-                        
-                        message = message.withUpdatedText(message.text + "\n" + noticeString)
-                        let noticeStringLength = noticeString.count
-                        let startIndex = originalTextLength + 1 // +1 for the newline character
-                        // Calculate the end index, which is the start index plus the length of noticeString
-                        let endIndex = startIndex + noticeStringLength
-
-                        var newAttributes = message.attributes
-                        newAttributes.append(
-                            TextEntitiesMessageAttribute(
-                                entities: [
-                                    MessageTextEntity(
-                                        range: startIndex..<endIndex,
-                                        // TODO(swiftgram): Add more instructions to collapsed block?
-                                        type: .BlockQuote(isCollapsed: false) //.Custom(type: ApplicationSpecificEntityType.Button)
-                                    )
-                                ]
-                            )
-                        )
-                        message = message.withUpdatedAttributes(newAttributes)
-                    }
-                    
-                    
                     if isMediaInverted {
                         var targetIndex = 0
                         if addedPriceInfo || addedPollMedia {
